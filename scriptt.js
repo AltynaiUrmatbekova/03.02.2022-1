439bc4d6be648cab3de2ff74bd395558db8f22e0
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-03 13:05:24 +0600

    javaScript numbers

---
+++ scriptt.js
@@ -0,0 +1,13 @@
+var emptyVariable;
+var message = "Hello World";
+var firstName, lastName = "Imashov";
+
+// Number
+// Javacstipt - математические операции не ведут к ошибке
+var exampleNumber1 = 123;
+var exampleNumber2 = 123.45;
+var exampleNumber3 = Infinity; 1\0
+var exampleNumber4 = Nan; Not a number
+// var example = "Elaman" \ 2;
+
+document.write(typeof exampleNumber1);
